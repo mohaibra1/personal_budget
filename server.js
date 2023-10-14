0d349f55e92ea2debdcb5ece64fac3fc912d952d
mohaibra <mimohadinho@gmail.com>
2023-10-14 23:48:25 +0200

    closes #3; Create your envelopes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express('')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+//database functions 
+const { getAllEnvelopes } = require('./server/db')
 
 //port
 const PORT = process.env.PORT || 4001 
@@ -8,11 +12,17 @@ const PORT = process.env.PORT || 4001
 //configure morgan
 app.use(morgan('dev'))
 
+//get all envelopes
 app.get('/', (req, res, next) => {
     console.log('Starting files....')
-    res.send({envelopes: 'Envelopes'})
+    res.send(getAllEnvelopes())
 })
 
+//create an envelope
+app.use('/', (req, res, next) => {
+    console.log('Creating new envelope....')
+    res.send()
+})
 
 //server listening port
 app.listen(PORT, () => {
