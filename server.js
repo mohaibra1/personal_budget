a3558f74ed179385d903224a680109f7ffe7cd61
mohaibra <mimohadinho@gmail.com>
2023-10-14 23:28:39 +0200

    closes #2; set up the enviroment and a simple endpoint

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express('')
+const morgan = require('morgan')
 
 //port
 const PORT = process.env.PORT || 4001 
+
+//configure morgan
+app.use(morgan('dev'))
 
 app.get('/', (req, res, next) => {
     console.log('Starting files....')
